42caa7f3f5f8937f517279d55255a1b4ac6b91fd
Michael Kever <michaelkever@gmail.com>
2018-08-02 11:08:39 -0500

    ConstantExercise
    For p.161 Tech Academy

---
+++ ConstantAndVarExercise/ConstantAndVarExercise/ConstantAndVarExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConstantAndVarExercise
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            const string fName = "Michael";
+            var lName = "Kever";
+            Console.WriteLine("{0} R. {1} is my name.", fName, lName);
+            Console.ReadLine();
+        }
+    }
+}
